ae6b13ca277a1b25ca66d04d065ac1adcf6b20e2
unknown <luckydos@gmail.com>
2020-05-15 14:09:23 +0900

    webExample Link Add

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+	document.getElementById('webExampleMoveButton').addEventListener('click', function() {
+		location.href='https://luckydos.github.io/webExample';
+	});
+};
